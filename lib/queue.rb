e66c31dce191e258cf0ad23aef5b3de5d8e8889a
= <=>
2021-02-28 00:01:15 -0300

    implement queue initialize

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,2 +1,11 @@
 class Queue
+    attr_accessor :head, :tail, :length
+
+    # Initialize an empty stack.
+    # Complexity: O(1).
+    def initialize
+        self.head   = nil
+        self.tail   = nil
+        self.length = 0
+    end
 end
--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -1,9 +1,12 @@
 require 'queue.rb'
 
 # Queue class tests
-RSpec.describe Queue do
+RSpec.describe 'Queue' do
     it 'creates a queue class' do 
       queue = Queue.new
       expect(queue).to be_kind_of(Queue)
+      expect(queue.head).to eq nil
+      expect(queue.tail).to eq nil
+      expect(queue.length).to eq 0
     end
 end
